56a99ae8c242bceb90a16f4931a3c5ed51d3d3e0
Sohii <alicesohii@outlook.com>
2024-11-24 12:33:47 +0800

    添加多个插件样本

--- plugins/sample_plugins_2/space.py
+++ plugins/sample_plugins_2/space.py
@@ -3,7 +3,7 @@ from project import logger
 info ={
     "mail" : "test@test.com",
 }
-plugin_info = PluginInfo(name = "sample_plugins_1", version="0.0.1",info = info)
+plugin_info = PluginInfo(name = "sample_plugins_2", version="0.0.2",info = info)
 plugin = Plugin(plugin_info)
 
 logger.info(f"开始初始化插件{plugin_info.name},{plugin_info.version} ")
--- plugins/sample_plugins_3/space.py
+++ plugins/sample_plugins_3/space.py
@@ -3,7 +3,7 @@ from project import logger
 info ={
     "mail" : "test@test.com",
 }
-plugin_info = PluginInfo(name = "sample_plugins_1", version="0.0.1",info = info)
+plugin_info = PluginInfo(name = "sample_plugins_3", version="0.0.3",info = info)
 plugin = Plugin(plugin_info)
 
 logger.info(f"开始初始化插件{plugin_info.name},{plugin_info.version} ")
